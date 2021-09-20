eb7cc7c62a853ade2303f8978c52494b3634545c
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-09-20 11:21:28 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const port = 3001;
 
 server.use(cors());
 server.use(express.json());
+server.use("/products", productsRouter);
 
 server.listen(port, () => {
   console.log("Server is running on port:" + port);
